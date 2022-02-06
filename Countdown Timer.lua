b4fb67b4674f21766e11261fd35d4fae9333cf7c
awk <self@awk.space>
2022-02-06 09:08:42 -0500

    Use native Reaper function to get the project length

--- Countdown Timer.lua
+++ Countdown Timer.lua
@@ -1,18 +1,6 @@
-function end_of_project()
-  project_end = 0
-  for i = 0, reaper.CountMediaItems()-1 do
-    item = reaper.GetMediaItem(0, i)
-    pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    item_end = pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-    if item_end > project_end then
-      project_end = item_end
-    end
-  end
-  return project_end
-end
-
 function update_timer()
-  reaper.SNM_SetDoubleConfigVar("projtimeoffs", -end_of_project())
+  length = reaper.GetProjectLength(-1)
+  reaper.SNM_SetDoubleConfigVar("projtimeoffs", -length)
   reaper.UpdateTimeline()
 end
 
